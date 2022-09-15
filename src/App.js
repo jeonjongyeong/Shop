34c3a09f01de415a872d4c412478fd69436b7536
jkjkhh <jkjkhh@gmail.com>
2022-09-15 13:30:45 +0900

    main 생성 및 이미지 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./Components/Navbar";
+import Main from "./Components/main";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Main />
     </div>
   );
 }
