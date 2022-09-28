16ba8519ff98bc447496ab059a6693a9f56cb5f7
jkjkhh <jkjkhh@gmail.com>
2022-09-28 23:55:05 +0900

    useEffect를 활용한 input에 숫자만 입력 가능 구현

--- src/Components/Detail.js
+++ src/Components/Detail.js
@@ -3,24 +3,28 @@ import { useParams } from "react-router-dom";
 
 function Detail(props) {
   let { id } = useParams();
-  // let findGoods = props.shoes.find(function (goods) {
-  //   return goods.id === id;
-  // });
-
-  useEffect(() => {});
 
   let [count, setCount] = useState(0);
-  let [chance, setChance] = useState(
-    <div className="alert alert-warning">2초 이내 구매시 할인</div>
-  );
+  // let [chance, setChance] = useState(true);
+  let [num, setNum] = useState("");
 
-  setTimeout(() => {
-    setChance("");
-  }, 2000);
+  useEffect(() => {
+    // let timer = setTimeout(() => {
+    //   setChance(false);
+    // }, 2000);
+    if (isNaN(num) == true) {
+      alert("문자는 입력이 되지 않습니다.");
+    }
+    // return () => {
+    //   clearTimeout(timer);
+    // };
+  }, []);
 
   return (
     <div className="container">
-      {chance}
+      {/* {chance == true ? (
+        <div className="alert alert-warning">2초 이내 구매시 할인</div>
+      ) : null} */}
       {count}
       <button
         onClick={() => {
@@ -37,6 +41,11 @@ function Detail(props) {
           />
         </div>
         <div className="col-md-6 mt-4">
+          <input
+            onChange={(e) => {
+              setNum(e.target.value);
+            }}
+          />
           <h4 className="pt-5">{props.shoes[id].title}</h4>
           <p>{props.shoes[id].content}</p>
           <p>{props.shoes[id].price}원</p>
